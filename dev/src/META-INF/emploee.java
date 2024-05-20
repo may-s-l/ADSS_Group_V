c451ae7f3cc4abdaf1e09355f22cfe546958b2b6
ShilatAlon <130230860+ShilatAlon@users.noreply.github.com>
2024-05-20 23:10:20 +0300

    starting the work

---
+++ dev/src/META-INF/emploee.java
@@ -0,0 +1,17 @@
+import java.until.*;
+import java.until.date;
+
+public class emploee {
+
+    private String name;
+    private String job;
+    private String bank_accuont;
+    private String ID;
+    private int salery;
+    private Date start_date;
+    private String
+    //הוסיף תנאי העסקה
+
+
+
+}
