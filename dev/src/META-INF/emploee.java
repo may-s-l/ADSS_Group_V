391d1d956d08f58e8e942a17c017f8c3e751b21c
ShilatAlon <130230860+ShilatAlon@users.noreply.github.com>
2024-05-24 12:34:17 +0300

    1

--- dev/src/META-INF/emploee.java
+++ dev/src/META-INF/emploee.java
@@ -1,17 +1,94 @@
-import java.until.*;
-import java.until.date;
+package domain;
 
-public class emploee {
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Employee {
 
     private String name;
-    private String job;
     private String bank_accuont;
     private String ID;
-    private int salery;
-    private Date start_date;
-    private String
-    //הוסיף תנאי העסקה
+    private Job job;
+    private Float Salery;
+    private LocalDate start_date;
+    private LocalDate end_date;
 
+
+
+    public Employee(String name, String bank_accuont, String ID, Job job, Float salery, LocalDate start_date) {
+        this.name = name;
+        this.bank_accuont = bank_accuont;
+        this.ID = ID;
+        this.job = job;
+        this.Salery = salery;
+        this.start_date=start_date;
+        this.end_date=null;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBank_accuont() {
+        return bank_accuont;
+    }
+
+    public void setBank_accuont(String bank_accuont) {
+        this.bank_accuont = bank_accuont;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public Job getJob() {
+        return job;
+    }
+
+    public void setJob(Job job) {
+        this.job = job;
+    }
+
+    public Float getSalery() {
+        return Salery;
+    }
+
+    public void setSalery(Float salery) {
+        Salery = salery;
+    }
+
+    public LocalDate getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(LocalDate start_date) {
+        this.start_date = start_date;
+    }
+    public LocalDate getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(LocalDate end_date) {
+        this.end_date = end_date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || o instanceof Employee) {
+            return false;
+        }
+        Employee othre = (Employee) o;
+        return this.getID()==othre.getID();
+    }
 
 
 }
