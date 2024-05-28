86a51a983ce377b6b8c481372270bded9f78daf4
ShilatAlon <130230860+ShilatAlon@users.noreply.github.com>
2024-05-28 21:39:27 +0300

    menu-constraint and employee

---
+++ dev/src/presentation/menus/ConstraintMenu.java
@@ -0,0 +1,52 @@
+package dev.src.presentation.menus;
+
+import dev.src.domain.Constraint;
+import dev.src.domain.Employee;
+
+import java.sql.Time;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.Scanner;
+
+public class ConstraintMenu {
+    public ConstraintMenu(Employee employee){
+        Scanner scanner = new Scanner(System.in);
+        Employee employee1=employee;
+
+
+        while (true) {
+            System.out.println("Welcome to Constraint manu!");
+            System.out.println("1. Add new constraint");
+            System.out.println("2. Edit constraint");
+            System.out.println("3. Delete constraint");
+            System.out.println("4. Show constraint");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+
+                case 1:
+                    System.out.print("Enter date: as year/day/month ");
+                    LocalDate date = LocalDate.parse(scanner.nextLine());
+                    System.out.print("Enter the start time: as hh:mm ");
+                    String start_t_string=scanner.nextLine() + ":00";
+                    Time start_t = Time.valueOf(start_t_string);
+                    System.out.print("Enter the end time: as hh:mm ");
+                    String end_t_string=scanner.nextLine() + ":00";
+                    Time end_t = Time.valueOf(end_t_string);
+                    Constraint new_constrain=new Constraint(employee1, date,start_t,end_t);
+                case 2:
+                    System.out.print("Enter date: as year/day/month ");
+                    LocalDate dateToEdit = LocalDate.parse(scanner.nextLine());
+                    //serch in list of constraint
+                case 3:
+                    System.out.print("Enter date: as year/day/month ");
+                    LocalDate dateToDelete = LocalDate.parse(scanner.nextLine());
+                    //serch in list of constraint
+                case 4:
+
+            }
+        }
+    }
+}
--- dev/src/presentation/menus/EmployeeMenu.java
+++ dev/src/presentation/menus/EmployeeMenu.java
@@ -3,15 +3,15 @@ package dev.src.presentation.menus;
 import dev.src.domain.Employee;
 
 import java.util.*;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class EmployeeMenu {
 
-    public EmployeeMenu(String ID, List<Employee> employees) {
+    public EmployeeMenu(String ID, List<Employee> employees, Employee employee) {
         Scanner scanner = new Scanner(System.in);
         String id=ID;
         List<Employee> employeeList=employees;
+        Employee employee1=employee;
 
 
         while (true) {
@@ -25,8 +25,20 @@ public class EmployeeMenu {
 
             switch (choice) {
                 case 1:
-                    System.out.println();
+                    System.out.println("name: "+ employee1.getName());
+                    System.out.println("bank account: "+employee1.getBank_accuont());
+                    System.out.println("your ID: "+ employee1.getID());
+                    System.out.println("your work role: "+employee1.getJob());
+                    System.out.println("Salary: "+ employee1.getSalery());
+                    System.out.println("your start of work date: "+employee1.getStart_date());
 
+                case 2:
+                    ConstraintMenu cons= new ConstraintMenu(employee1);
+
+                case 3:
+
+                default:
+                    System.out.println("Invalid choice. Please try again.");
             }
         }
     }
