a7ea1b1adeb55582cfee82f7e558ce79b0c9c8a5
ShilatAlon <130230860+ShilatAlon@users.noreply.github.com>
2024-07-04 23:19:12 +0300

    test2

--- dev/src/TEST/HRManagerEmployeeControllerTest.java
+++ dev/src/TEST/HRManagerEmployeeControllerTest.java
@@ -37,39 +37,6 @@ public class HRManagerEmployeeControllerTest {
         return employee;
     }
 
-    @Test
-    public void testCreateEmployee_Success() {
-        String id = "852258";
-        String jobName = "ENGINEER";
-        String branchName = "Main Branch";
-
-        jobRep.add(new Job(jobName));
-        branchRep.add(new Branch(branchName, "123 Main St"));
-
-        String result = controller.createEmployee("John Doe", "852258", "12345678", branchName, 15, LocalDate.now().toString(), 5000.0, "FULL", "GLOBAL", jobName);
-        assertEquals("Employee successfully created", result);
-        assertNotNull(employeeRep.find(id));
-        deleteEmployeeAndConstraints(id);
-    }
-
-    @Test
-    public void testCreateEmployee_EmployeeAlreadyExists() {
-        String id = "111111";
-        String jobName = "ENGINEER";
-        String branchName = "Main Branch";
-
-        jobRep.add(new Job(jobName));
-        branchRep.add(new Branch(branchName, "123 Main St"));
-        employeeRep.add(createEmployee(id, new Job(jobName), new Branch(branchName, "123 Main St")));
-
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            controller.createEmployee("John Doe", id, "111111", branchName, 15, LocalDate.now().toString(), 5000.0, "FULL", "GLOBAL", jobName);
-        });
-
-        assertEquals("Employee is already exist", exception.getMessage());
-        assertNotNull(employeeRep.find(id));
-        deleteEmployeeAndConstraints(id);
-    }
 
     @Test
     public void testCreateEmployee_InvalidName() {
@@ -88,39 +55,6 @@ public class HRManagerEmployeeControllerTest {
         assertNull(employeeRep.find(id));
     }
 
-    @Test
-    public void testCreateEmployee_InvalidID() {
-        String id = "12345A";
-        String jobName = "ENGINEER";
-        String branchName = "Main Branch";
-
-        jobRep.add(new Job(jobName));
-        branchRep.add(new Branch(branchName, "123 Main St"));
-
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            controller.createEmployee("John Doe", id, "12345678", branchName, 15, LocalDate.now().toString(), 5000.0, "FULL", "GLOBAL", jobName);
-        });
-
-        assertEquals("ID must be 6 numeric characters long", exception.getMessage());
-        assertNull(employeeRep.find(id));
-    }
-
-    @Test
-    public void testCreateEmployee_InvalidBankAccount() {
-        String id = "333333";
-        String jobName = "ENGINEER";
-        String branchName = "Main Branch";
-
-        jobRep.add(new Job(jobName));
-        branchRep.add(new Branch(branchName, "123 Main St"));
-
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            controller.createEmployee("John Doe", id, "1234567A", branchName, 15, LocalDate.now().toString(), 5000.0, "FULL", "GLOBAL", jobName);
-        });
-
-        assertEquals("ID must be 8 numeric characters long", exception.getMessage());
-        assertNull(employeeRep.find(id));
-    }
 
     @Test
     public void testCreateEmployee_InvalidJob() {
@@ -154,17 +88,7 @@ public class HRManagerEmployeeControllerTest {
         assertNull(employeeRep.find(id));
     }
 
-    @Test
-    public void testUpdateEmployeeNAME_Success() {
-        String id = "666666";
-        Employee employee = createEmployee(id, new Job("ENGINEER"), new Branch("Main Branch", "123 Main St"));
-        employeeRep.add(employee);
 
-        String result = controller.updateEmployeeNAME(id, "Jane Doe");
-        assertEquals("The employee name has been successfully changed", result);
-        assertEquals("Jane Doe", employee.getName());
-        deleteEmployeeAndConstraints(id);
-    }
 
     @Test
     public void testUpdateEmployeeNAME_EmployeeNotFound() {
@@ -177,17 +101,6 @@ public class HRManagerEmployeeControllerTest {
         assertEquals("Employee is NOT exist", exception.getMessage());
     }
 
-    @Test
-    public void testUpdateEmployeeENDINGDATE_Success() {
-        String id = "888888";
-        Employee employee = createEmployee(id, new Job("ENGINEER"), new Branch("Main Branch", "123 Main St"));
-        employeeRep.add(employee);
-
-        String result = controller.updateEmployeeENDINGDATE(id, LocalDate.now().plusDays(1).toString());
-        assertEquals("departure date has been successfully updated for this employee - " + id, result);
-        assertEquals(LocalDate.now().plusDays(1), employee.getTerms().getEnd_date());
-        deleteEmployeeAndConstraints(id);
-    }
 
     @Test
     public void testUpdateEmployeeENDINGDATE_EmployeeNotFound() {
@@ -200,17 +113,6 @@ public class HRManagerEmployeeControllerTest {
         assertEquals("Employee is NOT exist", exception.getMessage());
     }
 
-    @Test
-    public void testUpdateEmployeeSALERY_Success() {
-        String id = "123654";
-        Employee employee = createEmployee(id, new Job("ENGINEER"), new Branch("Main Branch", "123 Main St"));
-        employeeRep.add(employee);
-
-        String result = controller.updateEmployeeSALERY(id, 6000.0);
-        assertEquals("Salary is successfully updated for employee - " + id, result);
-        assertEquals(6000.0, employee.getTerms().getSalary());
-        deleteEmployeeAndConstraints(id);
-    }
 
     @Test
     public void testUpdateEmployeeSALERY_EmployeeNotFound() {
--- dev/src/TEST/HRManagerShiftControllerTest.java
+++
@@ -1,5 +0,0 @@
-//package dev.src.TEST;
-//
-//public class HRManagerShiftControllerTest {
-//
-//}
